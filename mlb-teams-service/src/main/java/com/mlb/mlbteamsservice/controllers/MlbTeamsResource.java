e0c78956e5011fd90ffed2dd87e40e70399e5d64
anthonygallegos <galleg46@uwm.edu>
2022-05-17 02:25:49 -0500

    revised models, working on connecting to api via spring boot

--- mlb-teams-service/src/main/java/com/mlb/mlbteamsservice/controllers/MlbTeamsResource.java
+++ mlb-teams-service/src/main/java/com/mlb/mlbteamsservice/controllers/MlbTeamsResource.java
@@ -2,43 +2,44 @@ package com.mlb.mlbteamsservice.controllers;
 
 import com.mlb.mlbteamsservice.models.MlbPlayer;
 import com.mlb.mlbteamsservice.models.MlbTeam;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping("/teams")
 public class MlbTeamsResource {
+    @Value("${api-key}")
+    private String apiKey;
+
+    RestTemplate restTemplate = new RestTemplate();
+    String url = "http://brew-roster-svc.us-e2.cloudhub.io/api";
 
     @RequestMapping("/")
-    public List<MlbTeam> getAllTeams(){
-        List<MlbTeam> teams = new ArrayList<>();
+    public MlbTeam getAllTeams(){
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("api-key", apiKey);
+        HttpEntity requestEntity = new HttpEntity<>(httpHeaders);
 
-        teams.add(new MlbTeam(133, "Athletics", "Oakland Athletics", "Oakland",
-                                "OAK", "null", "American League", "American League West"));
-        teams.add(new MlbTeam(158, "Brewers", "Milwaukee Brewers", "Milwaukee",
-                "MIL", "null", "National League", "National League Central"));
+        ResponseEntity<MlbTeam> response = restTemplate.exchange(url +"/teams", HttpMethod.GET, requestEntity, MlbTeam.class);
 
-        return teams;
+        return response.getBody();
     }
 
     @RequestMapping("/{teamId}/players")
     public List<MlbPlayer> getPlayers(@PathVariable("teamId") int teamId){
-        //test variable to see if teamId matches the brewers
-        int brewerId = 158;
+        List<MlbPlayer> teamPlayers = new ArrayList<>();
 
-        if (teamId == brewerId){
-            return Collections.singletonList(
-                    new MlbPlayer(592885, "Hardcoded Yelich")
-            );
-        }
-        else
-            return Collections.singletonList(
-                    new MlbPlayer(0000, "nothing nothing")
-            );
+        //return a getRequest to the api with the above mapping with rest template.
+        //
+        return teamPlayers;
     }
 }
