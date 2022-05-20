d0726b6c2ba6c0af18c37e670b142757f17f30fa
anthonygallegos <galleg46@uwm.edu>
2022-05-20 15:36:21 -0500

    added a header component

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import {HttpClientModule} from "@angular/common/http";
 import {MlbTeamService} from "./service/mlb-team-service/mlb-team.service";
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
